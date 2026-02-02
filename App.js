adefabc388b34adaeb248114108d8e383912e8fc
Chu Thi Van Anh <chuthivananh160305@gmail.com>
2026-02-02 10:35:49 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,88 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+/* ===== Component con: Button màu ===== */
+class ColorButton extends Component {
+  render() {
+    const { title, color, onPress } = this.props;
+
+    return (
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: color }]}
+        onPress={() => onPress(color, title)}
+      >
+        <Text
+          style={[
+            styles.buttonText,
+            { color: color === 'yellow' ? 'black' : 'white' },
+          ]}
+        >
+          {title}
+        </Text>
+      </TouchableOpacity>
+    );
+  }
 }
 
+/* ===== Component cha ===== */
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      backgroundColor: 'green',
+    };
+  }
+
+  // Callback đổi màu background + hiển thị thông báo
+  handleChangeColor = (color, title) => {
+    this.setState({
+      backgroundColor: color,
+    });
+    Alert.alert('Thông báo', `Bạn đã chọn màu ${title}`);
+  };
+
+  render() {
+    return (
+      <View
+        style={[
+          styles.container,
+          { backgroundColor: this.state.backgroundColor },
+        ]}
+      >
+        <ColorButton title="GREEN" color="green" onPress={this.handleChangeColor} />
+        <ColorButton title="BLUE" color="blue" onPress={this.handleChangeColor} />
+        <ColorButton title="BROWN" color="brown" onPress={this.handleChangeColor} />
+        <ColorButton title="YELLOW" color="yellow" onPress={this.handleChangeColor} />
+        <ColorButton title="RED" color="red" onPress={this.handleChangeColor} />
+        <ColorButton title="BLACK" color="black" onPress={this.handleChangeColor} />
+      </View>
+    );
+  }
+}
+
+export default App;
+
+/* ===== Style ===== */
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  button: {
+    paddingVertical: 14,
+    borderRadius: 8,
+    marginBottom: 12,
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
   },
 });
